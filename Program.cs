5faa541df120ef59ea63cfddf902ad4236529933
Cecilia Wendin <70585333+CeciliaWendin@users.noreply.github.com>
2022-01-25 10:28:26 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+var app = builder.Build();
+
+app.UseStaticFiles(); 
+app.UseRouting();
+
+app.MapRazorPages();
+
+app.Run();
+
